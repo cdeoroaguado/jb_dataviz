a31373a05d9fff18b5451baef88e57483d45f0cd
Carlos de Oro Aguado <43149030+cdeoroaguado@users.noreply.github.com>
2025-03-29 11:26:01 -0500

    Update app_lleno.py

--- app_lleno.py
+++ app_lleno.py
@@ -6,7 +6,8 @@ from io import BytesIO
 import requests
 
 # Cargar los datos
-url = 'https://raw.githubusercontent.com/cdeoroaguado/jb_dataviz/blob/main/Base_Financiero.xlsx'
+url = 'https://raw.githubusercontent.com/cdeoroaguado/jb_dataviz/master/Base_Financiero.xlsx'
+#url = "https://raw.githubusercontent.com/cdaniellopez8/financiero/master/Base_Financiero.xlsx"
 
 # Descargar el archivo
 response = requests.get(url)
